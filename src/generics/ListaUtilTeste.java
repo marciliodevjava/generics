04f96db8f8b23f4dd77adfcc23dd86d3f732dcca
m-arc <m-arc@192.168.1.8>
2022-05-05 08:26:25 -0300

    Class Generics Restrita

---
+++ src/generics/ListaUtilTeste.java
@@ -0,0 +1,25 @@
+package generics;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListaUtilTeste {
+
+	public static void main(String[] args) {
+
+		List<String> langs = Arrays.asList("JS ", "Php ", "C++");
+		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+		String ultimaLiguagem1 = (String) ListaUtil.getUltimo1(langs);
+		Integer ultimoNumero1 = (Integer) ListaUtil.getUltimo1(nums);
+
+		System.out.println(ultimaLiguagem1);
+		System.out.println(ultimoNumero1);
+
+		String ultimaLiguagem2 = ListaUtil.<String>getUltimo2(langs);
+		Integer ultimoNumero2 = ListaUtil.<Integer>getUltimo2(nums);
+
+		System.out.println(ultimaLiguagem2);
+		System.out.println(ultimoNumero2);
+	}
+}
